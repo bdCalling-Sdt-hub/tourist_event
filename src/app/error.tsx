f14d24756c086fdeefdf511cf4c4b2f870b528eb
shaharulsiyam <siyamoffice0273@gmail.com>
2024-10-31 15:48:44 +0600

    s

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,7 @@
 'use client'
- 
+
 import { useEffect } from 'react'
- 
+
 export default function Error({
   error,
   reset,
@@ -13,18 +13,21 @@ export default function Error({
     // Log the error to an error reporting service
     console.error(error)
   }, [error])
- 
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-900 text-white">
+      <div className="text-center p-6">
+        <h1 className="text-6xl font-bold text-red-500">Oops!</h1>
+        <h2 className="mt-4 text-3xl font-semibold text-white-200">Something went wrong!</h2>
+        <p className="mt-2 text-lg text-gray-500">An unexpected error has occurred. Please try again.</p>
+        
+        <button
+          onClick={() => reset()}
+          className="mt-8 px-8 py-3 bg-blue-500 hover:bg-blue-200 text-white font-medium rounded-md shadow-md transition-colors"
+        >
+          Try Again
+        </button>
+      </div>
     </div>
   )
 }
