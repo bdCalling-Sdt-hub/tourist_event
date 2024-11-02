afad168999d0a821d1db0e341cc3abbd8ac757d0
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-11-02 16:52:42 +0600

    auth pages

---
+++ src/app/(homeLayout)/profile/page.tsx
@@ -0,0 +1,12 @@
+import Profile from '@/components/ProfilePage/Client/profile'
+import React from 'react'
+
+const ProfilePage = () => {
+    return (
+        <div className='container mx-auto'>
+            <Profile />
+        </div>
+    )
+}
+
+export default ProfilePage
---
+++ src/components/ProfilePage/Client/Profile.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const Profile = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Profile
