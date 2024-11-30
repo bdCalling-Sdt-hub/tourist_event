6018bd9ec8831758fc3c3c023bd3d0b80a80c27b
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-11-30 10:06:45 +0600

    s

--- src/Utils/serverUrl.ts
+++ src/Utils/serverUrl.ts
@@ -1,4 +1,5 @@
-export const baseUrl = 'http://103.145.138.200:5000'
+// export const baseUrl = 'http://103.145.138.200:5000'
+export const baseUrl = 'http://159.65.187.252:6001'
 export const imageUrl = (url: string) => {
     if (!url) {
         return ""
