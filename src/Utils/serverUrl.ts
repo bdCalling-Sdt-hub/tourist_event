de8efef81b70bf13a657b866099e651031d4c583
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-12-27 18:55:00 +0600

    fix issues

--- src/Utils/serverUrl.ts
+++ src/Utils/serverUrl.ts
@@ -1,5 +1,5 @@
 // export const baseUrl = 'http://103.145.138.200:5000'
-export const baseUrl = 'http://167.71.179.42:5000'
+export const baseUrl = 'https://server.whatsupjaco.com'
 export const imageUrl = (url: string) => {
     if (!url) {
         return ""
